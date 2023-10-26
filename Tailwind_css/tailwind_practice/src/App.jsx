095342b0731f847747a0598a7ce6706d99cc4769
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-10-26 22:32:41 +0500

    added the features section in loopstudios

--- Tailwind_css/tailwind_practice/src/App.jsx
+++ Tailwind_css/tailwind_practice/src/App.jsx
@@ -1,10 +1,12 @@
 import ProjectsMain from "./projects/ProjectsMain";
 import Hero from "./projects/LoopStudios/Hero";
+import Features from "./projects/LoopStudios/Features";
 
 export default function App() {
   return (
     <div className="LoopStudios">
       <Hero />
+      <Features />
     </div>
   );
 }
--- Tailwind_css/tailwind_practice/src/projects/LoopStudios/Hero.jsx
+++ Tailwind_css/tailwind_practice/src/projects/LoopStudios/Hero.jsx
@@ -33,6 +33,9 @@ function Hero() {
           </li>
         </ul>
       </div>
+      <div className="max-w-lg mt-32 mb-32 p-4 font-sans text-4xl text-white uppercase border-2 md:p-10 md:m-32 md:mx-0 md:text-5xl">
+        <h1 className="font-bold">Immersive experiences that deliver</h1>
+      </div>
     </div>
   );
 }
