0062268b4234455d5e92d0ccd648cecdf9ab408c
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-10-27 09:34:47 +0500

    completed the creations component

--- Tailwind_css/tailwind_practice/src/projects/LoopStudios/Creations.jsx
+++ Tailwind_css/tailwind_practice/src/projects/LoopStudios/Creations.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import deepEarthDesktop from "./loopstudios-landing-page-main/images/desktop/image-deep-earth.jpg";
 import deepEarthMobile from "./loopstudios-landing-page-main/images/mobile/image-deep-earth.jpg";
+import nightArcadeDesktop from "./loopstudios-landing-page-main/images/desktop/image-night-arcade.jpg";
+import nightArcadeMobile from "./loopstudios-landing-page-main/images/mobile/image-night-arcade.jpg";
+import soccerTeamDesktop from "./loopstudios-landing-page-main/images/desktop/image-soccer-team.jpg";
+import soccerTeamMobile from "./loopstudios-landing-page-main/images/mobile/image-soccer-team.jpg";
+import grid from "./loopstudios-landing-page-main/images/desktop/image-grid.jpg";
+import gridMobile from "./loopstudios-landing-page-main/images/mobile/image-grid.jpg";
 
 function Creations() {
   return (
@@ -14,7 +20,7 @@ function Creations() {
           See All
         </button>
       </div>
-      <div className="flex flex-col justify-center w-full space-y-6 text-2xl uppercase md:flex-row md:justify-between">
+      <div className="flex flex-col justify-center w-full space-y-6 text-2xl uppercase md:flex-row md:justify-between md:space-y-0 md:space-x-6">
         {/* item 1 */}
         <div className="group w-full relative overflow-hidden md:w-1/4">
           <img
@@ -31,6 +37,57 @@ function Creations() {
           <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to gray-900 group-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 "></div>
           <h5 className="absolute bottom-4 text-white left-0 px-4 duration-200  md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
             Deep Earth
+          </h5>
+        </div>
+        <div className="group w-full relative overflow-hidden md:w-1/4">
+          <img
+            src={nightArcadeDesktop}
+            alt="deep-earth"
+            className="w-full hidden transition-all ease-in-out duration-500 transform group-hover:scale-110 md:block"
+          />
+          <img
+            src={nightArcadeMobile}
+            alt="deep-earth"
+            className="w-full block transition-all ease-in-out duration-500 transform group-hover:scale-110 md:hidden"
+          />
+          {/* items gradient */}
+          <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to gray-900 group-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 "></div>
+          <h5 className="absolute bottom-4 text-white left-0 px-4 duration-200  md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
+            Night Arcade
+          </h5>
+        </div>
+        <div className="group w-full relative overflow-hidden md:w-1/4">
+          <img
+            src={soccerTeamDesktop}
+            alt="deep-earth"
+            className="w-full hidden transition-all ease-in-out duration-500 transform group-hover:scale-110 md:block"
+          />
+          <img
+            src={soccerTeamMobile}
+            alt="deep-earth"
+            className="w-full block transition-all ease-in-out duration-500 transform group-hover:scale-110 md:hidden"
+          />
+          {/* items gradient */}
+          <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to gray-900 group-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 "></div>
+          <h5 className="absolute bottom-4 text-white left-0 px-4 duration-200  md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
+            Soccer Team VR
+          </h5>
+        </div>
+        <div className="group w-full relative overflow-hidden md:w-1/4">
+          <img
+            src={grid}
+            alt="deep-earth"
+            className="w-full hidden transition-all ease-in-out duration-500 transform group-hover:scale-110 md:block"
+          />
+          <img
+            src={gridMobile}
+            alt="deep-earth"
+            className="w-full block transition-all ease-in-out duration-500 transform group-hover:scale-110 md:hidden"
+          />
+          {/* items gradient */}
+          <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to gray-900 group-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 "></div>
+          <h5 className="absolute bottom-4 text-white left-0 px-4 duration-200  md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
+            The Grid
           </h5>
         </div>
       </div>
